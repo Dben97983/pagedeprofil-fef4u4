251dadc323ef2665ce7c55fcb1169a0613a1aa2f
Dben97983 <115337838+Dben97983@users.noreply.github.com>
2026-05-26 01:00:35 -0400

    Delete d1_dump.sql

--- d1_dump.sql
+++
@@ -1,7 +0,0 @@
-PRAGMA defer_foreign_keys=TRUE;
-CREATE TABLE _mocha_migrations (
-number     INTEGER UNIQUE,
-up_sql     TEXT NOT NULL,
-down_sql   TEXT NOT NULL,
-applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
